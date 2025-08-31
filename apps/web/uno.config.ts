bb8ccb82935305aa209e01d8e257ad4142919cbb
hongming.low <hongming.low@atoz-software.tech>
2025-09-01 02:20:05 +0800

    chore: remove unnecessary files

--- apps/web/uno.config.ts
+++ apps/web/uno.config.ts
@@ -1,30 +1,29 @@
-import { defineConfig } from '@unocss/vite';
-import transformerDirectives from '@unocss/transformer-directives';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-import presetWind3 from '@unocss/preset-wind3';
-import type { Theme } from '@unocss/preset-uno';
-import { presetSoybeanAdmin } from '@sa/uno-preset';
-import { themeVars } from './src/theme/vars';
+import { defineConfig } from "@unocss/vite";
+import transformerDirectives from "@unocss/transformer-directives";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import presetWind3 from "@unocss/preset-wind3";
+import { presetSoybeanAdmin } from "@sa/uno-preset";
+import { themeVars } from "./src/theme/vars";
 
-export default defineConfig<Theme>({
+export default defineConfig({
   content: {
     pipeline: {
-      exclude: ['node_modules', 'dist']
-    }
+      exclude: ["node_modules", "dist"],
+    },
   },
   theme: {
     ...themeVars,
     fontSize: {
-      'icon-xs': '0.875rem',
-      'icon-small': '1rem',
-      icon: '1.125rem',
-      'icon-large': '1.5rem',
-      'icon-xl': '2rem'
-    }
+      "icon-xs": "0.875rem",
+      "icon-small": "1rem",
+      icon: "1.125rem",
+      "icon-large": "1.5rem",
+      "icon-xl": "2rem",
+    },
   },
   shortcuts: {
-    'card-wrapper': 'rd-8px shadow-sm'
+    "card-wrapper": "rd-8px shadow-sm",
   },
   transformers: [transformerDirectives(), transformerVariantGroup()],
-  presets: [presetWind3({ dark: 'class' }), presetSoybeanAdmin()]
+  presets: [presetWind3({ dark: "class" }) as any, presetSoybeanAdmin() as any],
 });
